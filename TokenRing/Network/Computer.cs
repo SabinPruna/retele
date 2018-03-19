7d0f1d18c21e70f0b64a1799e48321dad23e6a8d
Sabin Pruna <sabinpruna@gmail.com>
2018-03-19 12:51:49 +0200

    Nearly functional non-thread token ring.

--- TokenRing/Network/Computer.cs
+++ TokenRing/Network/Computer.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TokenRing.Network {
     public class Computer {
-        public Computer() {
-            IP = GetRandomIpAddress();
+        private static int _messageCounter;
+
+        public Computer(Random random) {
+            IP = GetRandomIpAddress(random);
             Buffer = string.Empty;
         }
 
@@ -13,9 +17,29 @@ namespace TokenRing.Network {
 
         public string Buffer { get; set; }
 
-        private static string GetRandomIpAddress() {
-            Random random = new Random();
+        private static string GetRandomIpAddress(Random random) {
             return $"{random.Next(1, 255)}.{random.Next(0, 255)}.{random.Next(0, 255)}.{random.Next(0, 255)}";
+        }
+
+        public bool CopyIfDestination(Token token) {
+            if (token.DestinationIP == IP) {
+                token.IsDestinationReached = true;
+                Buffer += ";" + token.Message;
+                token.IsAvailable = true;
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public void LoadToken(Token token, IList<string> computersList) {
+            token.Message = $"This is message {_messageCounter++}";
+            token.IsDestinationReached = false;
+            token.IsAvailable = false;
+            token.SourceIP = IP;
+            token.DestinationIP = computersList.Where(i => i != IP).ToList().OrderBy(d => new Random().Next())
+                                               .FirstOrDefault();
         }
     }
 }
--- TokenRing/Program.cs
+++ TokenRing/Program.cs
@@ -1,21 +1,81 @@
-﻿using TokenRing.Network;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TokenRing.Network;
 
 namespace TokenRing {
-    internal class Program {
-        private static void Main() {
-            GenerateNetworkRing(5);
+    public class Program {
+        private static int _numberOfLoads = 4;
+
+        public static void Main() {
+            List<Computer> computerList = GenerateNetworkRing(10);
+            Token token = new Token();
+
+            while (_numberOfLoads != 0) {
+                //while (!token.IsAvailable) {
+                //    //get Co
+                //}
+
+                PrintBuffers(computerList);
+
+                Computer sourceComputer = computerList.OrderBy(d => new Random(DateTime.Now.Millisecond).Next()).First();
+                sourceComputer.LoadToken(token, computerList.Select(c => c.IP).ToList());
+                Console.WriteLine($"Sursa: {token.SourceIP} Destinatie: {token.DestinationIP}");
+
+                Computer computer = computerList.Find(x => x.IP == sourceComputer.NextIP);
+
+                while (!token.IsDestinationReached) {
+                    Console.WriteLine(computer.CopyIfDestination(token)
+                                          ? $"{computer.IP}: Am ajuns la destiantie"
+                                          : $"{computer.IP}: Muta jetonul");
+
+                    computer = computerList.Find(x => x.IP == computer.NextIP);
+                }
+
+                MoveTokenBackToSource(token, computerList);
+                PrintBuffers(computerList);
+                _numberOfLoads--;
+            }
         }
 
-        private static void GenerateNetworkRing(int numberOfComputers) {
-            Computer originalComputer = new Computer();
-            Computer lastComputer = new Computer();
+        private static void MoveTokenBackToSource(Token token, List<Computer> computerList) {
+            Computer computer = computerList.Find(x => x.IP == token.DestinationIP);
 
-            for (int i = 0; i < numberOfComputers - 1; i++) {
-                Computer computer = new Computer {NextIP = originalComputer.IP};
+            while (computer.NextIP != token.SourceIP) {
+                Console.WriteLine($"{computer.IP}: Muta jetonul");
+                computer = computerList.Find(x => x.IP == computer.NextIP);
+            }
+
+            Console.WriteLine($"{computer.NextIP}: Am ajuns inapoi");
+        }
+
+        private static List<Computer> GenerateNetworkRing(int numberOfComputers) {
+            Random random = new Random();
+
+            List<Computer> list = new List<Computer>();
+
+            Computer originalComputer = new Computer(random);
+            list.Add(originalComputer);
+            Computer computer = new Computer(random) {NextIP = originalComputer.IP};
+            list.Add(computer);
+            Computer lastComputer = new Computer(random);
+
+            for (int i = 0; i < numberOfComputers -2; i++) {
+                string lastIp = computer.IP;
+                computer = new Computer(random) {NextIP = lastIp};
                 lastComputer = computer;
+                list.Add(computer);
             }
 
             originalComputer.NextIP = lastComputer.IP;
+
+            return list;
+        }
+
+        public static void PrintBuffers(IList<Computer> computers) {
+            foreach (Computer computer in computers) {
+                Console.WriteLine($"{computer.IP} -> {computer.Buffer}");
+            }
         }
     }
 }
