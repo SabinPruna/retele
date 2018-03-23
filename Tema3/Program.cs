5dc37e880ea7f803e0e274d871a7f2009a9ba928
Sabin Pruna <sabin-catalin.pruna@student.unitbv.ro>
2018-03-23 23:44:13 +0200

    Finished 3rd homework.

---
+++ Tema3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Tema3.Utils;
+
+namespace Tema3 {
+    public class Program {
+        private static void Main() {
+
+            NetworkHelper.GetMachineName();
+
+            NetworkHelper.GetLocalIPAddress();
+
+            NetworkHelper.GetIPAddressesByName(Environment.MachineName);
+
+            NetworkHelper.WebPageToFile("https://www.google.ro/?gws_rd=cr&dcr=0&ei=DHW1Wr2mL4SrswH6vr_ADg");
+        }
+    }
+}
---
+++ Tema3/Utils/NetworkHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Tema3.Utils {
+    public class NetworkHelper {
+        public static void GetMachineName() {
+            Console.WriteLine($"Machine Name: {Environment.MachineName}");
+        }
+
+        public static string GetLocalIPAddress() {
+            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in host.AddressList) {
+                if (ip.AddressFamily == AddressFamily.InterNetwork ||
+                    ip.AddressFamily == AddressFamily.InterNetworkV6) {
+                    return ip.ToString();
+                }
+            }
+
+            return "No address found!";
+        }
+
+        public static void GetIPAddressesByName(string computerName) {
+            IPHostEntry host = Dns.GetHostEntry(computerName);
+            foreach (IPAddress ip in host.AddressList) {
+                Console.WriteLine(ip.ToString());
+            }
+        }
+
+        public static void WebPageToFile(string webpageUrl) {
+            WebClient client = new WebClient();
+            string content = client.DownloadString(webpageUrl);
+
+            client.DownloadFile(webpageUrl, @"E:\DirectWebPageDownload");
+
+            File.WriteAllText(@"E:\contentForTema3", content);
+        }
+    }
+}
