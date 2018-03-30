29648310907218d319f36f7b6ec5ca526f0917d7
Sabin Pruna <sabin-catalin.pruna@student.unitbv.ro>
2018-03-30 11:13:30 +0300

    Pseudo Function TokenRing.

--- TokenRing/Program.cs
+++ TokenRing/Program.cs
@@ -1,29 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-
 using TokenRing.Network;
 
-
-namespace TokenRing
-{
-    public class Program
-    {
+namespace TokenRing {
+    public class Program {
         #region Static Members
 
-        private static int _numberOfLoads = 4;
+        private static int _numberOfLoads = 40;
 
         #endregion
 
         #region Methods - Public
 
-        public static void Main()
-        {
-
+        public static void Main() {
             Token token = new Token();
-            List<Computer> computerList = GenerateNetworkRing(10);
+            List<Computer> computerList = GenerateNetworkRing(1000);
             List<Thread> computerThreads = GenerateComputerThreads(computerList, token);
             Random randomDistributor = new Random(Guid.NewGuid().GetHashCode());
             Computer sourceComputer = new Computer(randomDistributor);
@@ -49,7 +44,8 @@ namespace TokenRing
                     Thread.Sleep(1000);
                     sourceComputer = computerList.OrderBy(d => randomDistributor.Next()).First();
                     sourceComputer.LoadToken(token, computerList.Select(c => c.IP).ToList());
-                } else {
+                }
+                else {
                     sourceComputer = computerList.Find(c => c.IP == token.SourceIP);
                 }
 
@@ -60,8 +56,8 @@ namespace TokenRing
                 lock (token) {
                     while (!token.IsDestinationReached) {
                         Console.WriteLine(computer.CopyIfDestination(token)
-                            ? $"{computer.IP}: Am ajuns la destinatie"
-                            : $"{computer.IP}: Muta jetonul");
+                                              ? $"{computer.IP}: Am ajuns la destinatie"
+                                              : $"{computer.IP}: Muta jetonul");
 
                         computer = computerList.Find(x => x.IP == computer.NextIP);
                     }
@@ -75,8 +71,7 @@ namespace TokenRing
             PrintComputerBuffers(computerList);
         }
 
-        public static void PrintComputerBuffers(IList<Computer> computers)
-        {
+        public static void PrintComputerBuffers(IList<Computer> computers) {
             foreach (Computer computer in computers) {
                 Console.WriteLine($"{computer.IP} -> {computer.Buffer}");
             }
@@ -86,41 +81,46 @@ namespace TokenRing
 
         #region Methods - Private
 
-        private static void ComputerThreadsHandler(List<Computer> computerList, Token token)
-        {
+        private static void ComputerThreadsHandler(List<Computer> computerList, Token token) {
             Token temporaryToken = token;
             Parallel.ForEach(computerList,
-                computer => { new Thread(() => { computer.LoadToken(temporaryToken, computerList.Select(c => c.IP).ToList()); }).Start(); });
+                             computer => {
+                                 new Thread(() => {
+                                                computer.LoadToken(temporaryToken,
+                                                                   computerList.Select(c => c.IP).ToList());
+                                            }).Start();
+                             });
         }
 
-        private static List<Thread> GenerateComputerThreads(List<Computer> computerList, Token token)
-        {
+        private static List<Thread> GenerateComputerThreads(List<Computer> computerList, Token token) {
             List<Thread> computerThreads = new List<Thread>();
 
             foreach (Computer computer in computerList) {
                 Token temporaryToken = token;
-                Thread computerThread = new Thread(() => { computer.LoadToken(temporaryToken, computerList.Select(c => c.IP).ToList()); });
+                Thread computerThread = new Thread(() => {
+                                                       computer.LoadToken(
+                                                           temporaryToken, computerList.Select(c => c.IP).ToList());
+                                                   });
                 computerThreads.Add(computerThread);
             }
 
             return computerThreads;
         }
 
-        private static List<Computer> GenerateNetworkRing(int numberOfComputers)
-        {
+        private static List<Computer> GenerateNetworkRing(int numberOfComputers) {
             Random random = new Random();
 
             List<Computer> list = new List<Computer>();
 
             Computer originalComputer = new Computer(random);
             list.Add(originalComputer);
-            Computer computer = new Computer(random) { NextIP = originalComputer.IP };
+            Computer computer = new Computer(random) {NextIP = originalComputer.IP};
             list.Add(computer);
             Computer lastComputer = new Computer(random);
 
             for (int i = 0; i < numberOfComputers - 2; i++) {
                 string lastIp = computer.IP;
-                computer = new Computer(random) { NextIP = lastIp };
+                computer = new Computer(random) {NextIP = lastIp};
                 lastComputer = computer;
                 list.Add(computer);
             }
@@ -130,15 +130,14 @@ namespace TokenRing
             return list;
         }
 
-        private static void MoveTokenBackToSource(Token token, List<Computer> computerList)
-        {
+        private static void MoveTokenBackToSource(Token token, List<Computer> computerList) {
             Computer computer = computerList.Find(x => x.IP == token.DestinationIP);
 
             while (computer.NextIP != token.SourceIP) {
                 Console.WriteLine($"{computer.IP}: Muta jetonul");
                 computer = computerList.Find(x => x.IP == computer.NextIP);
             }
-
+            Debug.WriteLine("token finished");
             Console.WriteLine($"{computer.NextIP}: Am ajuns inapoi");
         }
 
